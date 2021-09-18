2bf1e70cfdc6d107e6d99bbefce0a7d42f0bc6bb
haeyonghahn <yong80211@gmail.com>
2021-09-18 17:59:57 +0900

    dataSource mariadb 로 수정

--- src/main/java/com/springframework/schedule/BootApplication.java
+++ src/main/java/com/springframework/schedule/BootApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
 	}
-
 }
--- src/main/java/com/springframework/schedule/job/QuartzJobFactory.java
+++ src/main/java/com/springframework/schedule/job/QuartzJobFactory.java
@@ -14,22 +14,6 @@ public class QuartzJobFactory implements Job {
 	public static List<ScheduleJob> jobList = Lists.newArrayList();
 
 	public static List<ScheduleJob> getInitAllJobs() {
-		for (int i = 0; i < 5; i++) {
-			ScheduleJob job = new ScheduleJob();
-			job.setJobId(String.valueOf(i));
-			job.setJobName("job_name_" + i);
-			if (i % 2 == 0) {
-				job.setJobGroup("job_group_even");
-			}
-			else {
-				job.setJobGroup("job_group_odd");
-			}
-			job.setJobStatus("1");
-			job.setCronExpression(String.format("0/%s * * * * ?", (i + 1) * 5));
-			job.setDesc("i am job number " + i);
-			job.setInterfaceName("interface" + i);
-			jobList.add(job);
-		}
 		return jobList;
 	}
 
@@ -39,13 +23,5 @@ public class QuartzJobFactory implements Job {
 		String jobName = scheduleJob.getJobName();
 
 		System.err.println(jobName);
-
-		if (jobName.equals("job_name_4") || jobName.equals("addjob")) {
-			try {
-				Thread.sleep(1000*60);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 }
